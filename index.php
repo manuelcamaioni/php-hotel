d9b78479a5ef6d722fc7c995df5f533d7e9ec952
manuelcamaioni <camaioni.manuel@gmail.com>
2023-07-03 16:07:55 +0200

    added form

--- index.php
+++ index.php
@@ -57,10 +57,18 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotels</title>
+    <!-- BOOTSTRAP v5.2 -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Bootstrap js -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
 
-    <ul>
+    <!-- <ul>
         <?php foreach($hotels as $hotel){ ?>
            <li>
                <?php foreach($hotel as $key => $value){ ?>
@@ -71,8 +79,26 @@ $hotels = [
                 <?php } ?>
            </li>
             
-       <?php } ?>
-    </ul>
+       <?php } ?> 
+
+    </ul>-->
+
+
+    <div class="wrapper d-flex justify-content-center w-50">
+        <form action="./server.php" method="GET" class="card w-100 p-2">
+        <div class="form-check d-flex justify-content-evenly w-100">
+            <label class="form-check-label" for="defaultCheck1">
+                Parcheggio
+            </label> 
+            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" name="parking">
+        </div>
+        <div class="d-flex justify-content-evenly w-100">
+            <label for="vote">Voto</label>
+            <input type="number" name="vote" id="" class="w-25">
+        </div>
+        <div class="btn-container p-2 d-flex justify-content-center"><button type="submit" class="btn btn-outline-success w-25">Cerca</button></div>
+        </form>
+    </div>
     
 </body>
 </html>
